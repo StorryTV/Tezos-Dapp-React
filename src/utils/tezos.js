5127e587248c157b503313012bdd54518fbd060e
Anshu Jalan <anshujalan206@gmail.com>
2022-06-07 15:30:53 +0530

    Update tezos.js

--- src/utils/tezos.js
+++ src/utils/tezos.js
@@ -2,7 +2,7 @@
 import { TezosToolkit } from "@taquito/taquito";
 import { wallet } from "./wallet";
 
-export const tezos = new TezosToolkit("https:/ithacanet.smartpy.io");
+export const tezos = new TezosToolkit("https://ithacanet.smartpy.io");
 
 // TODO 3 - Specify wallet provider for Tezos instance
 tezos.setWalletProvider(wallet);
