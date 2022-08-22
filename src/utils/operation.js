cba5696b8bb327b0e656408f1dcbaf2688eebe40
StorryTV <60582682+StorryTV@users.noreply.github.com>
2022-08-22 14:41:59 +0200

    Update operation.js

--- src/utils/operation.js
+++ src/utils/operation.js
@@ -3,8 +3,8 @@ import { tezos } from "./tezos";
 
 export const buyTicketOperation = async () => {
   try {
-    const contractInstance = await tezos.wallet.at("KT1JGp6cyFQPKprgJmHdfx1BKCAWvcscz6sR");
-    const op = await contractInstance.methods.buy_ticket().send({
+    const contractInstance = await tezos.wallet.at("KT1EDKhwYY9h8UuoxN6KpexoCT8xoaTRVAzv");
+    const op = await contractInstance.methods.tezToTokenPayment().send({
       amount: 1,
       mutez: false,
     });
@@ -17,8 +17,8 @@ export const buyTicketOperation = async () => {
 // TODO 10 - Call end_game entrypoint in the Lottery contract
 export const endGameOperation = async () => {
   try {
-    const contractInstance = await tezos.wallet.at("KT1JGp6cyFQPKprgJmHdfx1BKCAWvcscz6sR");
-    const op = await contractInstance.methods.end_game().send();
+    const contractInstance = await tezos.wallet.at("KT1EDKhwYY9h8UuoxN6KpexoCT8xoaTRVAzv");
+    const op = await contractInstance.methods.tokenToTezPayment().send();
     await op.confirmation(1);
   } catch (err) {
     throw err;
